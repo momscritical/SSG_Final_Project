d6ef82b7fc0a076699489fbc7160e2f0c75d0e0c
Taeyang <xodid3446@gmail.com>
2024-04-09 16:41:10 +0900

    test 240409

--- output.tf
+++ output.tf
@@ -1,21 +1,13 @@
-output "vpc_id" {
-  value = aws_vpc.vpc.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.vpc.id
+# }
 
-output "vpc_id" {
-  value = aws_vpc.default.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.default.id
+# }
 
-output "public_subnet_ids" {
-  value = [
-    module.public_subnet.subnet_id,
-  ]
-}
-
-output "private_subnet_ids" {
-  value = [
-    module.private_subnet_1.subnet_id,
-    module.private_subnet_2.subnet_id,
-    module.private_subnet_3.subnet_id,
-  ]
-}
+# output "public_subnet_ids" {
+#   value = [
+#     module.public_subnet.subnet_id,
+#   ]
+# }
