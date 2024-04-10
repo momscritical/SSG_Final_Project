02779144c5446089e2d75ba989dabbec78fc8876
Taeyang <xodid3446@gmail.com>
2024-04-10 22:25:06 +0900

    240410

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 terraform { 
   required_providers { 
     aws = { 
-      source = "hashicorp/aws" 
+      source  = "hashicorp/aws" 
       version = "~> 4.46.0" 
     } 
     random = { 
-      source = "hashicorp/random" 
+      source  = "hashicorp/random" 
       version = "~> 3.4.3" 
     } 
     tls = { 
-      source = "hashicorp/tls" 
+      source  = "hashicorp/tls" 
       version = "~> 4.0.4" 
     } 
     cloudinit = { 
-      source = "hashicorp/cloudinit" 
+      source  = "hashicorp/cloudinit" 
       version = "~> 2.2.0" 
     } 
     kubernetes = { 
-      source = "hashicorp/kubernetes" 
+      source  = "hashicorp/kubernetes" 
       version = "~> 2.16.1" 
     } 
   } 
@@ -31,6 +31,21 @@ provider "aws" {
 module "final_vpc" {
   source = "./modules/vpc"
 
-  vpc_cidr = var.vpc_cidr
-  vpc_name = var.vpc_name
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  web_subnet_cidr     = var.web_subnet_cidr
+  was_subnet_cidr     = var.was_subnet_cidr
+  db_subnet_cidr      = var.db_subnet_cidr
+
+  availability_zones  = var.availability_zones
+
+  vpc_name            = var.vpc_name
+  public_subnet_name  = var.public_subnet_name
+  web_subnet_name     = var.web_subnet_name
+  was_subnet_name     = var.was_subnet_name
+  db_subnet_name      = var.db_subnet_name
+  igw_name            = var.igw_name
+  ngw_name            = var.ngw_name
+  public_rt_name      = var.public_rt_name
+  private_rt_name     = var.private_rt_name
 }
--- variable.tf
+++ variable.tf
@@ -1,35 +1,12 @@
-variable "region" { 
-  description = "AWS region" 
-  type        = string 
-  default     = "ap-northeast-1" 
-}
-
-variable "vpc_name" {
-  type = string
-  default = "Final-VPC"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-1"
 }
 
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
-}
-
-variable "enable_dns_hostnames" {
-  description = "Whether to enable DNS hostnames for the VPC"
-  type        = bool
-  default     = true
-}
-
-variable "enable_dns_support" {
-  description = "Whether to enable DNS support for the VPC"
-  type        = bool
-  default     = true
-}
-
-variable "public_subnet_name" {
-    description = "Public Subnets name values"
-  type = string
-  default = "Bastion-Subnet"
 }
 
 variable "public_subnet_cidr" {
@@ -38,34 +15,16 @@ variable "public_subnet_cidr" {
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "web_subnet_name" {
-  description = "Web Subnets name values"
-  type = string
-  default = "Web-Subnet"
-}
-
 variable "web_subnet_cidr" {
   description = "Web Subnet CIDR values"
   type        = list(string)
   default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
-variable "was_subnet_name" {
-  description = "WAS Subnets name values"
-  type = string
-  default = "WAS-Subnet"
-}
-
 variable "was_subnet_cidr" {
   description = "WAS Subnet CIDR values"
   type        = list(string)
   default     = ["10.0.101.0/24", "10.0.102.0/24"]
-}
-
-variable "db_subnet_name" {
-  description = "Database Subnets name values"
-  type = string
-  default = "Database-Subnet"
 }
 
 variable "db_subnet_cidr" {
@@ -78,4 +37,69 @@ variable "availability_zones" {
   description = "Availability Zones"
   type        = list(string)
   default     = ["ap-northeast-1a", "ap-northeast-1c"]
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "Final-VPC"
+}
+
+variable "public_subnet_name" {
+  description = "Public Subnets name values"
+  type        = string
+  default     = "Bastion-Subnet"
+}
+
+variable "web_subnet_name" {
+  description = "Web Subnets name values"
+  type        = string
+  default     = "Web-Subnet"
+}
+
+variable "was_subnet_name" {
+  description = "WAS Subnets name values"
+  type        = string
+  default     = "WAS-Subnet"
+}
+
+variable "db_subnet_name" {
+  description = "DataBase Subnets name values"
+  type        = string
+  default     = "DB-Subnet"
+}
+
+variable "igw_name" {
+  description = "Internet Gateway name values"
+  type        = string
+  default     = "Internet-Gateway"
+}
+
+variable "ngw_name" {
+  description = "NAT Gateway name values"
+  type        = string
+  default     = "NAT-Gateway"
+}
+
+variable "public_rt_name" {
+  description = "Public Routing Table name values"
+  type        = string
+  default     = "Public-Routing-Table"
+}
+
+variable "private_rt_name" {
+  description = "Private Routing Table name values"
+  type        = string
+  default     = "Private-Routing-Table"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Whether to enable DNS hostnames for the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Whether to enable DNS support for the VPC"
+  type        = bool
+  default     = true
 }
