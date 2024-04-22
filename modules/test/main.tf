be3c479ad402d052a06f2150fe9332ebc1544bd2
Taeyang <xodid3446@gmail.com>
2024-04-22 16:50:14 +0900

    240422 test

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,5 +1,11 @@
 resource "null_resource" "modify_file" {
   provisioner "local-exec" {
-    command = "sed -i 's/INT_LB_DNS/${var.int_lb_dns}/g' ./yaml/before_nginx.conf > ./yaml/nginx.conf"
+    command = "sed -i 's/INT_LB_DNS/${var.int_lb_dns}/g' ./yaml/before_nginx.conf"
+  }
+}
+
+resource "null_resource" "modify_file" {
+  provisioner "local-exec" {
+    command = "cp ./yaml/before_nginx.conf > ./yaml/nginx.conf"
   }
 }
