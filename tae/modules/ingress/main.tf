f89cce716e53b47798c39c8f21d2d4496ff03008
Taeyang <xodid3446@gmail.com>
2024-04-29 17:29:53 +0900

    240426 Add Ingress Controller

--- tae/modules/ingress/main.tf
+++ tae/modules/ingress/main.tf
@@ -1,4 +1,4 @@
-# deploy.yaml 파일의 내용을 Terraform으로 정의
-resource "kubernetes_manifest" "ingress_nginx_controller" {
-  manifest = file("${var.yaml_location}")
-}
+# # deploy.yaml 파일의 내용을 Terraform으로 정의
+# resource "kubernetes_manifest" "ingress_nginx_controller" {
+#   manifest = file("${var.yaml_location}")
+# }
