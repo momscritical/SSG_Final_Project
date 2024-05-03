3067fdaa052cf696dd27ef6b4d5eef769207527e
Taeyang <xodid3446@gmail.com>
2024-05-03 15:29:10 +0900

    240503 Variable Test

--- tae/modules/sg/output.tf
+++ tae/modules/sg/output.tf
@@ -22,9 +22,9 @@ output "elb_sg_id" {
     value = aws_security_group.ext_lb.id
 }
 
-output "cluster_sg_id" {
-    value = aws_security_group.cluster.id
-}
+# output "cluster_sg_id" {
+#     value = aws_security_group.cluster.id
+# }
 
 # output "ilb_sg_id" {
 #     value = aws_security_group.int_lb.id
