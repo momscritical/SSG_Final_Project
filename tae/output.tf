30064baaf527445e5a5e9877f8f11afc257a30f0
Taeyang <xodid3446@gmail.com>
2024-05-03 15:09:20 +0900

    240503 Variable Test

--- tae/output.tf
+++ tae/output.tf
@@ -1,12 +1,14 @@
-# 클러스터의 엔드포인트 정보 출력
-output "cluster_endpoint" {
-  value = module.final_eks.cluster_endpoint
-}
+# # 클러스터의 엔드포인트 정보 출력
+# output "cluster_endpoint" {
+#   value = module.final_eks.cluster_endpoint
+# }
 
-# kubectl 명령어를 사용하여 클러스터에 연결하는 데 필요한 인증 기관 데이터 출력
-output "kubeconfig-certificate-authority-data" {
-  value = module.final_eks.kubeconfig-certificate-authority-data
-}
+# # kubectl 명령어를 사용하여 클러스터에 연결하는 데 필요한 인증 기관 데이터 출력
+# output "kubeconfig-certificate-authority-data" {
+#   value = module.final_eks.kubeconfig-certificate-authority-data
+# }
+
+
 
 # # 클러스터 토큰 값 출력
 # output "cluster_token" {
