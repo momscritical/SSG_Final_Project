bf64c3f2a08f716f2013586dc3b285c891a4c275
Taeyang <xodid3446@gmail.com>
2024-05-05 15:01:07 +0900

    240505 IRSA Test

--- tae/modules/ingress_controller/main.tf
+++ tae/modules/ingress_controller/main.tf
@@ -3,11 +3,16 @@ resource "null_resource" "install_ingress_ctroller" {
   provisioner "local-exec" {
     command = "kubectl apply -f ${var.yaml_location}"
   }
-}
 
-resource "null_resource" "unstall_ingress_ctroller" {
   provisioner "local-exec" {
     when    = destroy
     command = "kubectl delete -f ${var.yaml_location}"
   }
 }
+
+# resource "null_resource" "unstall_ingress_ctroller" {
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = "kubectl delete -f ${var.yaml_location}"
+#   }
+# }
