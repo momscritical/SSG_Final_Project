57eb9a91cb0781d4f837afa5b594ab3a5683e7b9
Taeyang <xodid3446@gmail.com>
2024-05-08 16:51:59 +0900

    240508 k8s test

--- tae/main.tf
+++ tae/main.tf
@@ -195,7 +195,7 @@ module "final_eks" {
 module "final_ingress_controller" {
   source        = "./modules/ingress_controller"
 
-  # yaml_location = var.ingress_controller_yaml
+  yaml_location = var.ingress_controller_yaml
   depends_on    = [ module.final_eks ]
 }
 
